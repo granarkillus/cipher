e2f4fb36094d82d7d196f60828c9eba3a4de7991
granarkillus <markhamgartley@gmail.com>
2026-06-12 23:25:40 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,5 +3,12 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias['@/lib/supabase'] = require('path').resolve('./lib/supabase.ts');
+    return config;
+  },
 };
 module.exports = nextConfig;
